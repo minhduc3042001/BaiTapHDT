7e315867e2d0099e2245a3ca227bd4f95f391197
minhduc3042001 <hoangduc3042001@gmail.com>
2021-09-27 21:49:29 +0700

    initial commit

--- src/com/duchm2008110312/lab1/Phan1Bai1.java
+++ src/com/duchm2008110312/lab1/Phan1Bai1.java
@@ -1,5 +1,14 @@
 package com.duchm2008110312.lab1;
-
+import java.util.Scanner;
 public class Phan1Bai1 {
+public static void main(String[] args) {
+        Scanner dieuKhien;
+        dieuKhien = new Scanner(System.in);
+        System.out.print("Họ và tên: ");
+        String hoTen = dieuKhien.nextLine();
+        System.out.print("Điểm TB: "); 
+        double diemTB = dieuKhien.nextDouble();   
+        System.out.printf("%s %f điểm", hoTen, diemTB);   
+        }
+    }
     
-}

3ce9564d2ebe17d0f3a5aa1e6865dbb5bb3aa1b6
minhduc3042001 <hoangduc3042001@gmail.com>
2021-09-27 22:04:48 +0700

    initial commit

--- src/com/duchm2008110312/lab1/Phan1bai2.java
+++ src/com/duchm2008110312/lab1/Phan1bai2.java
@@ -1,5 +1,19 @@
 package com.duchm2008110312.lab1;
-
-public class Phan1bai2 {
-    
+import java.util.Scanner;
+public class Phan1bai2 { 
+          public static void main (String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập vào chiều dài của hình chữ nhật:");
+        double dai = scanner.nextDouble();
+        System.out.println("Nhập vào chiều rộng của hình chữ nhật:");
+        double rong = scanner.nextDouble();
+        double chuVi = (dai + rong)*2;
+        System.out.println("Chu vi hình chữ nhật là:"+chuVi);
+        double dienTich = dai*rong;
+        System.out.println("Diện tích hình chữ nhật là:"+dienTich);
+        double canhNhoNhat = Math.min(dai,rong);
+        System.out.println("Cảnh nhỏ nhất hình chữ nhật là:"+canhNhoNhat);
+        System.out.println("------------------------------------------");
+    }
 }

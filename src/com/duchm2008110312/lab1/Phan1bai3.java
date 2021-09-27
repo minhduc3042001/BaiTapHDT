b25ab957982004dd63a10e58873fd51f0b94b730
minhduc3042001 <hoangduc3042001@gmail.com>
2021-09-27 22:13:29 +0700

    initial commit

---
+++ src/com/duchm2008110312/lab1/Phan1bai3.java
@@ -0,0 +1,17 @@
+package com.duchm2008110312.lab1;
+import java.util.Scanner;
+public class Phan1bai3 {
+    public static void main (String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập vào chiều dài của khối lập phương:");
+        double dai = scanner.nextDouble();
+        System.out.println("Nhập vào chiều rộng của khối lập phương:");
+        double rong = scanner.nextDouble();
+        System.out.println("Nhập vào chiều cao của khối lập phương:");
+        double cao = scanner.nextDouble();
+        double theTichLapPhuong = dai*rong*cao;
+        System.out.println("Thế Tích Khối Lập Phương là:"+theTichLapPhuong);
+    }
+    
+}

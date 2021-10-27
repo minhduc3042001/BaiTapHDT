1d6239d795719281cfbf3cfba87ef5028173dc1c
minhduc3042001 <hoangduc3042001@gmail.com>
2021-10-27 23:19:27 +0700

    bai tap

---
+++ src/com/duchm2008110312/tuan7/Shape.java
@@ -0,0 +1,18 @@
+package com.duchm2008110312.tuan7;
+
+public class Shape {
+    boolean nhapChuot;
+    String amThanh;
+    public static int soLanNhapChuot;
+    Void xoay(){
+        System.out.println("Xoay 360 theo chiều kim đồng hồ");
+    }
+    Void phatAmThanh(){
+        System.out.println("Ting Ting");
+    }
+    Square(boolean n, String s){
+        nhapChuot = n;
+        amThanh = s;
+        soLanNhapChuot++;
+    }
+}
---
+++ src/com/duchm2008110312/tuan7/SquareTestDrive.java
@@ -0,0 +1,18 @@
+package com.duchm2008110312.tuan7;
+
+public class SquareTestDrive {
+    public static void main(String[] args){
+        Square square = new Square(true, "hinhTamGiac.AIF");
+
+        System.out.println("Hình có được nhấp chuột: "+square.nhapChuot);
+
+        System.out.println("Số lần nhấp chuột: "+Square.soLanNhapChuot);
+
+        square.xoay();
+
+        System.out.println("Âm thanh phát ra khi được nhấp chuột vào: "+square.amThanh);
+
+        square.phatAmThanh();
+    }
+    
+}
